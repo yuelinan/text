f0ceea24984e324d1f4320f930cad6e4b2f2493d
yuelinan <15930303321@163.com>
2017-08-22 16:31:02 +0800

    one
    加了一个项目

---
+++ hello/hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 岳立楠 on 2017/8/22.
+//  Copyright © 2017年 岳立楠. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
